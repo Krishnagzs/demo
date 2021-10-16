a9dd344ab678d4fa31c263fb3fb71d86e8373f9b
Krishnagzs <70741357+Krishnagzs@users.noreply.github.com>
2021-10-16 23:45:59 +0530

    first commit

---
+++ krishna.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class krishna
+{
+public static void main(String args[])
+{
+	Scanner ob=new Scanner(System.in);
+		
+String c="y";
+while (c.equals("y"))
+ {
+ 	System.out.println("good morning krishna");
+  	System.out.println("Do you continue for press y/n");
+  	c=ob.nextLine();
+}
+}
+}
---
+++ string1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class string1
+{
+public static void main(String args[])
+{
+	String n,m,p;
+	n="krishna";
+	m="kumar";
+	p=n+" "+m;
+System.out.println(p);
+System.out.println(p.toUpperCase());
+System.out.println(p.toLowerCase());
+System.out.println( "hello="  +p.toUpperCase());
+String u="krityaveer";
+int l=u.length();
+System.out.println("length="+l);
+
+
+
+
+}
+}
